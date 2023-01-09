e04cf0ebcfdf72c128a239253fbf8d12e3fcadc8
Oliver Earl <oliver.earl@stickee.co.uk>
2023-01-09 16:26:08 +0000

    test: First test to ensure Instrumentation is working correctly under Orchestra and Pest.

---
+++ tests/InstrumentTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Stickee\Instrumentation\Databases\DatabaseInterface;
+use Stickee\Instrumentation\Databases\Log;
+use Stickee\Instrumentation\Databases\NullDatabase;
+use Stickee\Instrumentation\Instrument;
+
+beforeEach(function (): void {
+    Instrument::setDatabase(new NullDatabase());
+});
+
+test('set database will change the internal database implementation', function (): void {
+    Instrument::setDatabase(new Log('test.log'));
+
+    // Check whether the internal database has actually changed using black magic:
+    $bypass = static function (): DatabaseInterface {
+        /** @noinspection PhpExpressionAlwaysNullInspection */
+        return static::$database;
+    };
+
+    $bypass = $bypass->bindTo(null, Instrument::class);
+    $database = $bypass();
+
+    expect($database)
+        ->toBeInstanceOf(DatabaseInterface::class)
+        ->toBeInstanceOf(Log::class);
+});
