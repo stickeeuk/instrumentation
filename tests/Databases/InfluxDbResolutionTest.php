d0f59b20643edbecc30907f90861e623eb74c814
Oliver Earl <oliver.earl@stickee.co.uk>
2023-01-16 16:25:03 +0000

    test: Use Pest syntax.

--- tests/Databases/InfluxDbResolutionTest.php
+++ tests/Databases/InfluxDbResolutionTest.php
@@ -14,11 +14,10 @@ it('can be resolved by the service container by automatically fetching the dsn f
 });
 
 it('will throw an error when attempting to resolve dsn automatically if the config is not set', function (): void {
-    $this->expectException(Exception::class);
     Config::set('instrumentation.dsn');
 
     app(InfluxDb::class);
-});
+})->throws(Exception::class);
 
 it('will automatically retrieve ssl verification settings from config when resolved by the service container', function (): void {
     Config::set('instrumentation.dsn', $this::EXAMPLE_DSN);
