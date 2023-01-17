f3b449d72adee69d7dd5c4bbfcb373f1608477ef
Oliver Earl <oliver.earl@stickee.co.uk>
2023-01-17 12:20:36 +0000

    test: Refactor tests.

--- tests/InstrumentTest.php
+++ tests/InstrumentTest.php
@@ -2,14 +2,10 @@
 
 declare(strict_types=1);
 
+
 use Stickee\Instrumentation\Databases\DatabaseInterface;
 use Stickee\Instrumentation\Databases\Log;
-use Stickee\Instrumentation\Databases\NullDatabase;
 use Stickee\Instrumentation\Instrument;
-
-beforeEach(function (): void {
-    Instrument::setDatabase(new NullDatabase());
-});
 
 test('set database will change the internal database implementation', function (): void {
     Instrument::setDatabase(new Log('test.log'));
